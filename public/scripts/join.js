5a131fd12931ae8ec62d0ee7e9434987b7755d30
menghaoyu <menghaoyu2002@gmail.com>
2022-06-14 01:04:49 -0400

    added join page

--- public/scripts/join.js
+++ public/scripts/join.js
@@ -1,6 +1,42 @@
-if (!localStorage.getItem('username')) {
+if (!sessionStorage.getItem('username')) {
     document.location.href = '/';
 }
 
 const mainText = document.querySelector('h1');
-mainText.innerText = `Hi ${localStorage.getItem('username')}!`;
+const changeName = document.querySelector('.changeName');
+const options = document.querySelector('.options');
+const joinButton = options.firstElementChild;
+const createButton = options.lastElementChild;
+const roomCodeForm = document.querySelector('form');
+const backButton = document.querySelector("input[value='Back']");
+const goButton = document.querySelector("input[value='Go'");
+
+mainText.innerText = `Hi ${sessionStorage.getItem('username')}!`;
+
+changeName.onclick = () => {
+    sessionStorage.removeItem('username');
+    document.location.href = '/';
+};
+
+joinButton.onclick = () => {
+    console.log('join clicked');
+    options.style.display = 'none';
+    roomCodeForm.style.display = 'block';
+};
+
+createButton.onclick = () => {
+    console.log('create clicked');
+    document.location.href = '/chat.html';
+};
+
+backButton.onclick = (event) => {
+    event.preventDefault();
+    options.style.display = 'flex';
+    roomCodeForm.style.display = 'none';
+};
+
+goButton.onclick = (event) => {
+    event.preventDefault();
+    const roomid = document.querySelector("input[type='text']").value;
+    document.location.href = `/chat.html?roomid=${roomid}`;
+};
